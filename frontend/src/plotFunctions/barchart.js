c7c42295a0fe9a8625fe24f8c1b02ec5f4db7715
KobeVdL <kobe.vanderlinden@student.kuleuven.be>
2023-04-05 20:14:57 +0200

    added bar plots

---
+++ frontend/src/plotFunctions/barchart.js
@@ -0,0 +1,50 @@
+import React, { useRef, useEffect } from "react";
+import * as d3 from "d3";
+
+function BarChart({ data }) {
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const w = 400;
+    const h = 300;
+
+    const svg = d3
+      .select(svgRef.current)
+      .attr("width", w)
+      .attr("height", h)
+      .style("overflow", "visible")
+      .style("margin-top", "75px");
+
+    const xScale = d3
+      .scaleBand()
+      .domain(data.map((value, index) => index))
+      .range([0, w])
+      .padding(0.5);
+
+    const yScale = d3.scaleLinear().domain([0, d3.max(data)]).range([h, 0]);
+
+    const xAxis = d3.axisBottom(xScale).ticks(data.length);
+
+    const yAxis = d3.axisLeft(yScale).ticks(5);
+
+    svg
+      .append("g")
+      .call(xAxis)
+      .attr("transform", `translate(0,${h})`);
+
+    svg.append("g").call(yAxis);
+
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("x", (value, index) => xScale(index))
+      .attr("y", (value) => yScale(value))
+      .attr("width", xScale.bandwidth())
+      .attr("height", (value) => h - yScale(value));
+  }, [data]);
+
+  return <svg ref={svgRef}></svg>;
+}
+
+export default BarChart;
