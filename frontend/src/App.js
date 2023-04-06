b1a529e6a7b0a39a845214348320de90f5cf017a
KobeVdL <kobe.vanderlinden@student.kuleuven.be>
2023-04-06 12:02:55 +0200

    Created template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 
-import React, {useState, useRef, useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import * as d3 from 'd3'
 import './App.css';
+import { Card, Layout, Space } from 'antd';
 import BarChart from './plotFunctions/barchart';
+
 
 function App() {
 
-  const [data] = useState([25, 50, 35, 15, 94, 10]);
+  /*const [data] = useState([25, 50, 35, 15, 94, 10]);
 
   const charts = [];
 
@@ -14,13 +16,96 @@ function App() {
   for (let i = 0; i < 8; i++) {
     charts.push(<BarChart key={i} data={data} />);
   }
+  
 
   return (
     <div className="App">
       <div className="row">{charts.slice(0, 4)}</div>
       <div className="row">{charts.slice(4, 8)}</div>
     </div>
+  );*/
+  const { Header, Footer, Sider, Content } = Layout;
+  const headerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    height: 64,
+    paddingInline: 50,
+    lineHeight: '64px',
+    backgroundColor: '#7dbcea',
+  };
+  const contentStyle = {
+    textAlign: 'center',
+    minHeight: 120,
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#108ee9',
+  };
+  const siderStyle = {
+    textAlign: 'center',
+    lineHeight: '120px',
+    color: '#fff',
+    backgroundColor: '#3ba0e9',
+  };
+  const footerStyle = {
+    textAlign: 'center',
+    color: '#fff',
+    backgroundColor: '#7dbcea',
+  };
+
+
+  // flex duidt de relatieve grootte aan van de elementen, de container gaat proberen de elementen even groot te maken
+  // Een hogere flex waarde tegenover de andere gaat dus meer ruimte innemen
+
+  //TODO relatieve groottes   <Card style={{ flex: 1, height: "300px", backgroundColor: "lightgray", marginLeft: "10px", marginRight: "10px" }}> cool </Card>
+  return (
+    <div className="App">
+
+      <Space
+        direction="vertical"
+        style={{
+          width: '100%',
+        }}
+        size={[0, 48]}
+      ></Space>
+
+      <h1>Verkeersongevallen in België</h1>
+      <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "20px" }} >
+        <div style={{ flex: 1.2, flexDirection: "column", height: "800px", marginLeft: "10px", marginRight: "10px", display: "flex", justifyContent: "space-between" }}>
+          <Card style={{ flex: 2, height: "550px", backgroundColor: "lightgray", marginBottom: "20px" }}> cool </Card>
+          <Card style={{ flex: 1, height: "200px", backgroundColor: "lightgray" }}> cool </Card>
+        </div>
+
+        <div style={{ flex: 1, flexDirection: "column", height: "800px", marginLeft: "10px", marginRight: "10px", display: "flex", justifyContent: "space-between" }}>
+          <div style={{ flex: 1,  height: "200px" , display: "flex", justifyContent: "space-between" }}>
+            <Card style={{ flex: 1, height: "200px", backgroundColor: "lightgray"}}> cool </Card>
+            <Card style={{ flex: 1, height: "200px", backgroundColor: "lightgray" }}> cool </Card>
+          </div>
+          <div style={{ flex: 1,  height: "200px", display: "flex", justifyContent: "space-between" }}>
+            <Card style={{ flex: 1, height: "200px", backgroundColor: "lightgray"}}> cool </Card>
+            <Card style={{ flex: 1, height: "200px", backgroundColor: "lightgray" }}> cool </Card>
+          </div>
+          <div style={{ flex: 1,  height: "200px" , display: "flex", justifyContent: "space-between" }}>
+            <Card style={{ flex: 1, height: "200px", backgroundColor: "lightgray"}}> cool </Card>
+            <Card style={{ flex: 1, height: "200px", backgroundColor: "lightgray" }}> cool </Card>
+          </div>
+          <div style={{ flex: 1,  height: "200px" , display: "flex", justifyContent: "space-between" }}>
+            <Card style={{ flex: 1, height: "200px", backgroundColor: "lightgray"}}> cool </Card>
+            <Card style={{ flex: 1, height: "200px", backgroundColor: "lightgray" }}> cool </Card>
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+    </div>
   );
+
+
+
+
+
 }
 
 export default App;
