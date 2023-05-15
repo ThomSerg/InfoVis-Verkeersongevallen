004120fb0c83e3ce973605d7d299a18cd707c9c2
Thomas <thomas.s2000@hotmail.com>
2023-05-15 18:45:29 +0200

    Extract SVG creation and add hover effect

--- frontend/src/plotFunctions/StackedBarChart.js
+++ frontend/src/plotFunctions/StackedBarChart.js
@@ -1,23 +1,49 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 import data2 from "../europe_gov.csv";
 
 function StackedBarChart({cat}) {
 
-  const svgRef = useRef();
+  // Reference to the SVG
+  const svgRef = useRef(null);
+  // State holding the initialised SVG
+  const [svg, setSvg] = useState(null);
+
+  // State holding a lock for graph updates
+  const [updateLock, setUpdateLock] = useState(false);
+
+
+
+
+  var margin = { top: 10, right: 30, bottom: 30, left: 40 };
+  const barHeight = 100;
+  var halfBarHeight = barHeight / 2;
+  var width = 600 - margin.left - margin.right;
+  var height = 300 - margin.top - margin.bottom;
+
+
+
+  /* * * * * * * * * * 
+   * Create the SVG  *
+   * * * * * * * * * */
+  useEffect(() => {
+    // append the svg object to the body of the page
+    setSvg(d3.select(svgRef.current)
+              .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            );
+  }, [])
+
 
   useEffect(() => {
-    const margin = { top: 10, right: 30, bottom: 30, left: 40 };
-    const height =200;
-    const barHeight = 100;
-    const halfBarHeight = barHeight / 2;
-    const w = 600 - margin.left - margin.right;
-    const h = 300 - margin.top - margin.bottom;
+    
+    if (svg) {
 
     const colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33"];
     
-    const svg = d3.select(svgRef.current);
-
     d3.csv(data2).then(data => { 
       
         const rollupData = d3.rollups(data, v => v.length, d => d[cat])
@@ -58,7 +84,7 @@ function StackedBarChart({cat}) {
 
         const xScale = d3.scaleLinear()
       .domain([0, total])
-      .range([0, w]);
+      .range([0, width]);
 
     const join = svg.selectAll('g')
       .data(groupData)
@@ -68,35 +94,58 @@ function StackedBarChart({cat}) {
     join.append('rect')
       .attr('class', 'rect-stacked')
       .attr('x', d => xScale(d.cumulative))
-      .attr('y', h / 2 - halfBarHeight)
+      .attr('y', height / 2 - halfBarHeight)
       .attr('height', barHeight)
       .attr('width', d => xScale(d.value))
-      .style('fill', (d, i) => colors[i]);
+      .style('fill', (d, i) => colors[i])
+
+      .on('mouseover', function(event, d) {
+        setUpdateLock(true);
+
+
+        svg.selectAll('.rect-stacked').filter(function(rs) {return rs.label != d.label}).style('opacity', '.5');
+        
+        // .each(function(rs,i) {
+        //   if (rs.label != d.label) {
+        //     console.log(rs);
+        //     rs.style('opacity', '.5');
+        //   }
+        // })
+      })
+
+      .on('mouseout', function(event, d) {
+        svg.selectAll('.rect-stacked').filter(function(rs) {return rs.label != d.label}).style('opacity', '1');
+
+        setUpdateLock(false);
+      })
 
     join.append('text')
       .attr('class', 'text-value')
       .attr('text-anchor', 'middle')
       .attr('x', d => xScale(d.cumulative) + (xScale(d.value) / 2))
-      .attr('y', (h / 2) + 5)
+      .attr('y', (height / 2) + 5)
       .text(d => d.value);
 
     join.append('text')
       .attr('class', 'text-percent')
       .attr('text-anchor', 'middle')
       .attr('x', d => xScale(d.cumulative) + (xScale(d.value) / 2))
-      .attr('y', (h / 2) - (halfBarHeight * 1.1))
+      .attr('y', (height / 2) - (halfBarHeight * 1.1))
       .text(d => d3.format('.1f')(d.percent) + ' %');
 
     join.append('text')
       .attr('class', 'text-label')
       .attr('text-anchor', 'middle')
       .attr('x', d => xScale(d.cumulative) + (xScale(d.value) / 2))
-      .attr('y', (h / 2) + (halfBarHeight * 1.1) + 20)
+      .attr('y', (height / 2) + (halfBarHeight * 1.1) + 20)
       .style('fill', (d, i) => colors[i])
       .text(d => d.label);
 
+      
+
     });
-  }, [cat]);
+  }
+  }, [cat, svg]);
 
   return (
     <svg ref={svgRef} width="600" height="500"/>
